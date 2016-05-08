fa1963b26924ba17e12f5a4af5e82ac2659e085e
KarasawaTakahiro <tkhr.karasawa@gmail.com>
2016-05-08 17:11:12 +0900

    add     ファイルを読んでinnerHTMLに加える関数の同期版

--- front/assembler.js
+++ front/assembler.js
@@ -29,6 +29,28 @@ assembler.loadAndInnerHtml = function loadAndInnerHtml(dom, file, encoding, errC
     });
 };
 
+/*
+ * ファイルからテキストを読んでHTMLとしてdomに当てはめる
+ * 同期版
+ *
+ * dom:         dom.innerHTMlにファイルの内容を当てはめる
+ * file:        読み込むファイルパス
+ * encoding:    ファイルのエンコード[utf08]
+ */
+assembler.loadAndInnerHtmlSync = function loadAndInnerHtml(dom, file, encoding, errCb){
+    encoding = typeof encoding !== "undefined" ? encoding : "utf-8";
+    if(typeof encoding === "function"){
+        errCb = encoding;
+        encoding = "utf-8";
+    }
+
+    var data = fs.readFileSync(file, {encoding: encoding});
+    if(data){
+        dom.innerHTML = data;
+    }else{
+        errCb("can't read html file");
+    }
+};
 
 
 module.exports = assembler;
