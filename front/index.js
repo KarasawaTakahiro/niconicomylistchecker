dd9274f7ddb5708b5238be3c017334bba607a038
KarasawaTakahiro <tkhr.karasawa@gmail.com>
2016-05-08 16:42:57 +0900

    add     index.htmlにパーツをはめ込んだ

--- front/index.js
+++ front/index.js
@@ -4,6 +4,7 @@ var remote = require('remote');
 
 var client = require("../front/client");
 var common = require("../front/common");
+var assembler = require("../front/assembler");
 
 var constants = remote.require("./lib/constants");
 
@@ -24,4 +25,19 @@ window.onload = function(){
     client.movieList(0);
 
     common.bind_my_funcs();
+
+    assembler.loadAndInnerHtml(document.getElementById("frame_left"),
+            "./view/parts/feed_list.html", function(err){
+                console.log(err);
+            });
+
+    assembler.loadAndInnerHtml(document.getElementById("frame_right"),
+            "./view/parts/dashboard.html", function(err){
+                console.log(err);
+            });
+
+    assembler.loadAndInnerHtml(document.getElementById("frame_bottom"),
+            "./view/parts/toolbox.html", function(err){
+                console.log(err);
+            });
 };
